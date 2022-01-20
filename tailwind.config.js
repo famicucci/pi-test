688b0b595aa5b84c180267b6c3a0d0d81ecff793
Francisco Micucci <famicucci@gmail.com>
2022-01-20 15:28:22 -0300

    Init Vite and Taildwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	content: [
+		'./index.html',
+		'./src/**/*.{react,js,ts,jsx,tsx}',
+		'./components/**/*.{react,js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
