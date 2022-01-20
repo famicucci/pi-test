07e93338f519a595ded819c93643694b05995461
Francisco Micucci <famicucci@gmail.com>
2022-01-20 19:57:52 -0300

    Create Card

---
+++ components/Card.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Button from './Button';
+
+const Card = (props) => {
+	const { name, height, gender } = props;
+
+	return (
+		<div className="flex border-solid border-gray-200 border-2 p-4 rounded-lg mb-2">
+			<div className="grow">
+				<h3 className="text-xl font-semibold">{name}</h3>
+				<p>
+					Heigh: <span>{height}</span>
+				</p>
+				<p>
+					Gender: <span>{gender}</span>
+				</p>
+			</div>
+			<div>
+				<Button content="Delete" />
+			</div>
+		</div>
+	);
+};
+
+export default Card;
--- components/CardList.jsx
+++ components/CardList.jsx
@@ -1,27 +1,6 @@
 import React, { useContext, useEffect } from 'react';
-import Button from './Button';
 import PeopleContext from '../context/PeopleContext';
-
-const Card = (props) => {
-	const { name, height, gender } = props;
-
-	return (
-		<div className="flex border-solid border-gray-200 border-2 p-4 rounded-lg mb-2">
-			<div className="grow">
-				<h3 className="text-xl font-semibold">{name}</h3>
-				<p>
-					Heigh: <span>{height}</span>
-				</p>
-				<p>
-					Gender: <span>{gender}</span>
-				</p>
-			</div>
-			<div>
-				<Button content="Delete" />
-			</div>
-		</div>
-	);
-};
+import Card from './Card';
 
 const CardList = () => {
 	const { people, getPeople } = useContext(PeopleContext);
