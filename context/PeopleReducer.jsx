c067f721f427ee24a5105cf10f2aeeeaee8e879b
Francisco Micucci <famicucci@gmail.com>
2022-01-20 22:02:48 -0300

    Get people from api

--- context/PeopleReducer.jsx
+++ context/PeopleReducer.jsx
@@ -2,11 +2,11 @@ import { GET_PEOPLE } from '../types';
 
 const ClientesReducer = (state, action) => {
 	switch (action.type) {
-		// case GET_PEOPLE:
-		// 	return {
-		// 		...state,
-		// 		people: action.payload,
-		// 	};
+		case GET_PEOPLE:
+			return {
+				...state,
+				people: action.payload,
+			};
 		default:
 			return state;
 	}
--- context/PeopleState.jsx
+++ context/PeopleState.jsx
@@ -5,28 +5,20 @@ import swapiAxios from '../config/swapiAxios';
 
 import { GET_PEOPLE } from '../types';
 
-const people = [
-	{ name: 'Owen Lars', height: 179, gender: 'male' },
-	{ name: 'Beru Whitesun lars', height: 159, gender: 'female' },
-	{ name: 'Mike Tower', height: 172, gender: 'male' },
-	{ name: 'M Lars', height: 163, gender: 'female' },
-];
-
 const PeopleState = (props) => {
 	const initialState = {
-		people: people,
+		people: [],
 	};
 
 	const [state, dispatch] = useReducer(PeopleReducer, initialState);
 
 	const getPeople = async () => {
 		const r = await swapiAxios.get('/api/people/');
-		console.log(r.data.results);
 
-		// dispatch({
-		// 	type: GET_PEOPLE,
-		// 	payload: r.data.results,
-		// });
+		dispatch({
+			type: GET_PEOPLE,
+			payload: r.data.results,
+		});
 	};
 
 	return (
